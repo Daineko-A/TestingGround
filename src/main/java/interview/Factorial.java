d653830c43426d3f541d4ca13f0ad9513fc708da
Ploskiy <alexandr.daineko@gmail.com>
2022-07-27 23:46:09 +0300

    without comment.

---
+++ src/main/java/interview/Factorial.java
@@ -0,0 +1,19 @@
+package interview;
+
+import java.util.List;
+
+public class Factorial {
+
+    public static void main(String[] args) {
+        System.out.println(factorial(10));
+    }
+
+    private static int factorial(int i) {
+//       return i != 1 ? i * factorial(--i) : 1;
+        if (i != 1) {
+            return i * factorial(--i);
+        } else {
+            return 1;
+        }
+    }
+}
---
+++ src/main/java/interview/Sets.java
@@ -0,0 +1,73 @@
+package interview;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class Sets {
+
+    public static void main(String[] args) {
+
+        Set<User> userSet = new TreeSet<>();
+//        Set<User> userSet = new HashSet<>();
+
+        User userOne = new User("1", "1");
+//        User userTwo = new User("1", "1");
+        User userTwo = new User("2", "2");
+
+        userSet.add(userOne);
+        userSet.add(userTwo);
+
+        System.out.println(userSet);
+
+        userOne.setFirstName(userTwo.getFirstName());
+        userOne.setLastName(userTwo.getLastName());
+
+        System.out.println(userSet);
+
+    }
+
+
+    public static class User implements Comparable<User> {
+        private String firstName;
+        private String lastName;
+
+        public User(String firstName, String lastName) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public String getFirstName() {
+            return firstName;
+        }
+
+        public void setFirstName(String firstName) {
+            this.firstName = firstName;
+        }
+
+        public String getLastName() {
+            return lastName;
+        }
+
+        public void setLastName(String lastName) {
+            this.lastName = lastName;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "firstName='" + firstName + '\'' +
+                    ", lastName='" + lastName + '\'' +
+                    '}';
+        }
+
+        @Override
+        public int compareTo(User o) {
+            int i = getFirstName().compareTo(o.getFirstName());
+            if (i != 0) {
+                return i;
+            }
+            return getLastName().compareTo(o.getLastName());
+        }
+    }
+}
