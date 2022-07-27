a595cec1b2e5814e9fa425e330ccbf91efa6af94
Ploskiy <alexandr.daineko@gmail.com>
2022-07-27 16:42:49 +0300

    without comment.

---
+++ Common/sqlScripts/Music_store.sql
@@ -0,0 +1,72 @@
+-- CREATE DATABASE movie_store;
+USE movie_store;
+CREATE TABLE artists (id INT AUTO_INCREMENT, first_name VARCHAR(50) NOT NULL, last_name VARCHAR(50) UNIQUE NOT NULL, birth DATE NOT NULL, PRIMARY KEY(id));
+
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Timothée', 'Chalamet', '1995-01-27');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Matthew', 'McConaughey', '1969-11-04');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Rebecca', 'Ferguson', '1983-10-19');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Colin', 'Farrell', '1976-05-31');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Charlie', 'Hunnam', '1980-04-10');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Leonardo', 'DiCaprio', '1974-11-11');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Joseph', 'Gordon-Levitt', '1981-02-17');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Marion', 'Cotillard', '1975-09-30');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Jeremy', 'Renner', '1971-01-07');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Anne', 'Hathaway', '1982-11-12');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Mila', 'Kunis', '1983-08-14');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Brad', 'Pitt', '1963-12-18');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Johnny', 'Depp', '1963-06-09');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Matt', 'Damon', '1970-10-08');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Mark', 'Wahlberg', '1971-06-05');
+INSERT INTO artists (first_name, last_name, birth) VALUES ('Tom', 'Hanks', '1956-07-09');
+
+CREATE TABLE awards (id INT AUTO_INCREMENT, title VARCHAR(50) NOT NULL, date YEAR NOT NULL, artist_id INT, PRIMARY KEY(id), FOREIGN KEY(artist_id) REFERENCES artists(id));
+INSERT INTO awards (title, artist_id, date) VALUES ('Golden Globe', 13, 2008 );
+INSERT INTO awards (title, artist_id, date) VALUES ('Oscar', 14, 1998);
+INSERT INTO awards (title, artist_id, date) VALUES ('Oscar', 6, 2016 );
+INSERT INTO awards (title, artist_id, date) VALUES ('Oscar', 2, 2014 );
+INSERT INTO awards (title, artist_id, date) VALUES ('Oscar', 12, 2020 );
+INSERT INTO awards (title, artist_id, date) VALUES ('Oscar', 12, 2014 );
+INSERT INTO awards (title, artist_id, date) VALUES ('Oscar', 10, 2013 );
+INSERT INTO awards (title, artist_id, date) VALUES ('Oscar', 8, 2008 );
+INSERT INTO awards (title, artist_id, date) VALUES ('Golden Globe', 4, 2009 );
+INSERT INTO awards (title, artist_id, date) VALUES ('Oscar', 16, 1995 );
+INSERT INTO awards (title, artist_id, date) VALUES ('Oscar', 16, 1994 );
+
+CREATE TABLE movies (id INT AUTO_INCREMENT, title VARCHAR(50) UNIQUE NOT NULL, release_date YEAR NOT NULL, PRIMARY KEY(id));
+INSERT INTO movies (title, release_date) VALUES ('Interstellar', 2014);
+INSERT INTO movies (title, release_date) VALUES ('The Martian', 2015);
+INSERT INTO movies (title, release_date) VALUES ('The Departed', 2006);
+INSERT INTO movies (title, release_date) VALUES ('Inception', 2010);
+INSERT INTO movies (title, release_date) VALUES ('Shutter Island', 2009);
+INSERT INTO movies (title, release_date) VALUES ('Pirates of the Caribbean', 2003);
+INSERT INTO movies (title, release_date) VALUES ('Fantastic Beasts and Where to Find Them', 2016);
+INSERT INTO movies (title, release_date) VALUES ('Uncharted', 2022);
+INSERT INTO movies (title, release_date) VALUES ('Ted', 2012);
+INSERT INTO movies (title, release_date) VALUES ('Friends with Benefits', 2011);
+INSERT INTO movies (title, release_date) VALUES ('Fight Club', 1999);
+INSERT INTO movies (title, release_date) VALUES ('Snatch', 2000);
+INSERT INTO movies (title, release_date) VALUES ('The Curious Case of Benjamin Button', 2008);
+INSERT INTO movies (title, release_date) VALUES ('Forrest Gump', 1994);
+INSERT INTO movies (title, release_date) VALUES ('Catch Me If You Can', 2002);
+INSERT INTO movies (title, release_date) VALUES ('The Green Mile', 1999);
+INSERT INTO movies (title, release_date) VALUES ('Finch', 2021);
+
+CREATE TABLE artists_movies (artist_id INT NOT NULL, movie_id INT NOT NULL, PRIMARY KEY(artist_id, movie_id), FOREIGN KEY(artist_id) REFERENCES artists(id), FOREIGN KEY(movie_id) REFERENCES movies(id));
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (16 ,17);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (16 ,16);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (16 ,14);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (12 ,12);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (12 ,11);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (11 ,10);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (6 ,15);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (6 ,4);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (6 ,3);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (14 ,3);
+INSERT INTO artists_movies (artist_id, movie_id) VALUES (15 ,3);
+
+# DROP
+-- DROP DATABASE DATABASE movie_store;
+-- DROP TABLE artists;
+-- DROP TABLE awards;
+-- DROP TABLE movies;
+-- DROP TABLE artists_movies;
---
+++ Common/sqlScripts/Scripts_for_Music_store.sql
@@ -0,0 +1,55 @@
+SELECT * FROM artists;
+SELECT * FROM awards;
+SELECT * FROM movies;
+SELECT * FROM artists_movies;
+
+SELECT id, first_name AS name, last_name, birth  FROM artists;
+
+# 
+SELECT id, first_name AS name, last_name, ((YEAR(CURRENT_DATE) - YEAR(birth)) - (DATE_FORMAT(CURRENT_DATE, '%m%d') < DATE_FORMAT(birth, '%m%d'))) AS age FROM artists;
+-- SELECT id, first_name AS name, last_name, $date = ((YEAR(CURRENT_DATE) - YEAR(birth)) - (DATE_FORMAT(CURRENT_DATE, '%m%d') < DATE_FORMAT(birth, '%m%d'))) AS age FROM artists where 'date' > 40;
+
+# LIMIT
+SELECT id, first_name AS name, last_name, birth  FROM artists LIMIT 2;
+SELECT id, first_name AS name, last_name, birth  FROM artists LIMIT 2 OFFSET 3;
+-- SELECT id, first_name AS name, last_name, birth  FROM artists LIMIT 2 OFFSET 2*2;
+
+# ORDER BY DESC 
+SELECT id, first_name AS name, last_name, birth  FROM artists ORDER BY name, id DESC  ;
+
+# DISTINCT
+SELECT DISTINCT first_name FROM artists; 
+
+# COUNT(), MAX(), MIN(), AVG(), SUM()
+SELECT COUNT(*) AS count FROM artists;
+SELECT MAX(birth) FROM artists; 
+
+# UPDATE 
+UPDATE  artists SET first_name = 'Mark_uptd' WHERE first_name = 'Mark'; 
+UPDATE  artists SET first_name = 'Mark' WHERE first_name = 'Mark_uptd'; 
+
+# DELETE 
+DELETE FROM  awards where id = 8;
+
+#
+SELECT aw.artist_id FROM awards aw WHERE aw.date = '2008' LIMIT 1;
+SELECT a.first_name, a.last_name  FROM artists a WHERE a.id = (SELECT aw.artist_id FROM awards aw WHERE aw.date = '2008' LIMIT 1);
+SELECT a.first_name, a.last_name  FROM artists a WHERE a.first_name like '%on%';
+
+SELECT a.first_name, a.last_name, aw.title , aw.date  FROM artists a, awards aw WHERE a.id = aw.artist_id;
+
+SELECT aw.artist_id FROM awards aw GROUP BY aw.artist_id;	
+SELECT a.first_name, a.last_name FROM artists a where a.id = ( SELECT aw.artist_id FROM awards aw WHERE COUNT(aw.artist_id));
+
+SELECT aw.date FROM awards aw;
+SELECT SUM(w.date) from (SELECT date FROM awards) AS w;
+
+SELECT COUNT(aw.id) from awards aw inner join artists a on aw.artist_id = a.id where a.first_name = 'Brad';
+
+
+SELECT a.first_name, a.last_name, mv.title from movies mv 
+join artists_movies am on mv.id = am.movie_id 
+JOIN artists a on a.id = am.artist_id 
+WHERE a.FIRST_NAME like '%leo%';
+
+
