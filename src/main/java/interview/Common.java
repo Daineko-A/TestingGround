e7ec351b2dfede578e435343e4de6beb3e68ce00
Ploskiy <alexandr.daineko@gmail.com>
2022-07-29 14:12:57 +0300

    without comment.

--- src/main/java/interview/Common.java
+++ src/main/java/interview/Common.java
@@ -1,9 +1,9 @@
-package tutorials.loops;
+package interview;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 
-public class ForLoop {
+public class Common {
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
         System.out.println("Sec: " + TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis() - start));
