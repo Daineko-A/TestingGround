347bfc29257eeb49d342c678e39262b83a001232
Ploskiy <alexandr.daineko@gmail.com>
2022-08-10 16:58:04 +0300

    without comment.

--- src/main/java/interview/Consumer.java
+++ src/main/java/interview/Consumer.java
@@ -10,32 +10,24 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Consumer {
-    /**
-         * Called periodically to consume an integer.
-     */
     static final Long PERIOD = TimeUnit.MINUTES.toMillis(1);
     private static final Map<Long, List<Integer>> acceptedInt = new LinkedHashMap<>();
 
-    public static void accept( int number ) {
+    public static void accept(int number) {
         acceptedInt.merge(
                 System.currentTimeMillis(),
                 List.of(number),
                 (oldVal, newVal) -> Stream.concat(oldVal.stream(), newVal.stream()).collect(Collectors.toList()));
-    };
+    }
 
-    /**
-     * Returns the mean (aka average) of numbers consumed in the
-     * last 5 minute period.
-     */
-
-    public static Double mean(){
+    public static Double mean() {
         long startTime = System.currentTimeMillis() - PERIOD;
         List<Integer> numbers = new ArrayList<>();
 
         Iterator<Map.Entry<Long, List<Integer>>> iterator = acceptedInt.entrySet().iterator();
-        while (iterator.hasNext()){
+        while (iterator.hasNext()) {
             Map.Entry<Long, List<Integer>> next = iterator.next();
-            if(next.getKey() > startTime) {
+            if (next.getKey() > startTime) {
                 numbers.addAll(next.getValue());
             } else {
                 acceptedInt.remove(next.getKey());
@@ -46,7 +38,7 @@ public class Consumer {
                 .mapToDouble(d -> d)
                 .average()
                 .orElse(0.0);
-    };
+    }
 
     public static void main(String[] args) {
         accept(1);
