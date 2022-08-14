468337df35e9d861c961f6e51a6a3dabc5758522
Ploskiy <alexandr.daineko@gmail.com>
2022-08-14 23:13:00 +0300

    without comment.

---
+++ src/main/java/common/collectionsTests/SetTests.java
@@ -0,0 +1,32 @@
+package common.collectionsTests;
+
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+import static org.junit.Assert.assertEquals;
+
+public class SetTests {
+
+    @Test
+    public void differentSets() {
+        Set<String> setOne = new HashSet<>();
+        Set<String> setTwo = new TreeSet<>();
+
+        String s1 = "one";
+        String s2 = "two";
+        String s3 = "three";
+
+        setOne.add(s1);
+        setOne.add(s2);
+        setOne.add(s3);
+
+        setTwo.add(s1);
+        setTwo.add(s2);
+        setTwo.add(s3);
+
+        assertEquals(setOne, setTwo);
+    }
+}
