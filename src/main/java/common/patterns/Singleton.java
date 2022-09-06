af7d17856b9482f502c783c3da4c622d8390024b
Ploskiy <alexandr.daineko@gmail.com>
2022-09-06 23:11:02 +0300

    commit

---
+++ src/main/java/common/patterns/Singleton.java
@@ -0,0 +1,16 @@
+package common.patterns;
+
+public class Singleton {
+    private static Singleton instance;
+
+    private Singleton() {
+    }
+
+    private static Singleton getInstance() {
+        if(instance == null) {
+            instance = new Singleton();
+        }
+
+        return instance;
+    }
+}
