cf8a289a9e942182268fb390cfbe0fda1abb1bd0
Ploskiy <alexandr.daineko@gmail.com>
2022-09-16 12:04:08 +0300

    commit

--- src/main/java/algorithms/searches/Binary.java
+++ src/main/java/algorithms/searches/Binary.java
@@ -10,28 +10,34 @@ public class Binary {
 
     @Test
     public void binaryTest() {
-        int limit = 10;
+        int limit = 100000;
+        for (int i = 0; i < 20; i++) {
         int[] arbitraryArrayInt = ArbitraryArray.getArbitraryArrayInt(limit);
         int element = arbitraryArrayInt[new Random().nextInt(limit - 1)];
         Arrays.sort(arbitraryArrayInt);
-
-        System.out.println(element + " : " + binary(arbitraryArrayInt, element));
+            System.out.println(element + " : " + binary(arbitraryArrayInt, element));
+        }
     }
 
     private int binary(int[] array, int element) {
         int minPosition = 0;
         int maxPosition = array.length - 1;
         int mid = 0;
+        int iteeration = 0;
         while (minPosition <= maxPosition) {
             mid = (minPosition + maxPosition) / 2;
             if (element == array[mid]) {
-                return array[mid];
+                iteeration++;
+                break;
             } else if (element < array[mid]) {
+                iteeration++;
                 maxPosition = mid - 1;
             } else if (element > array[mid]) {
+                iteeration++;
                 minPosition = mid + 1;
             }
         }
+        System.out.println("iterations: " + iteeration);
         return array[mid];
     }
 }
---
+++ src/main/java/algorithms/sorts/MergeSort.java
@@ -0,0 +1,58 @@
+package algorithms.sorts;
+
+import algorithms.utils.ArbitraryArray;
+import org.junit.Test;
+
+public class MergeSort {
+
+    @Test
+    public void mergeSortTest() {
+        for (int element : mergeSort(ArbitraryArray.getArbitraryArrayInt(10))) {
+            System.out.println(element);
+        }
+    }
+
+    private int[] mergeSort(int[] array) {
+        if (array.length < 2) {
+            return array;
+        }
+
+        int mid = array.length / 2;
+        int midPosition = array.length - mid;
+        int[] arrayLeft = new int[mid];
+        int[] arrayRight = new int[midPosition];
+
+        for (int i = 0; i < mid; i++) {
+            arrayLeft[i] = array[i];
+        }
+        for (int i = mid; i < array.length; i++){
+            arrayRight[i - mid] = array[i];
+        }
+
+        arrayLeft = mergeSort(arrayLeft);
+        arrayRight = mergeSort(arrayRight);
+
+        return merge(array, arrayLeft, arrayRight, mid, array.length - mid);
+    }
+
+    private int[] merge(int[] array, int[] arrayLeft, int[] arrayRight, int left, int right) {
+
+        int i = 0, j = 0, k = 0;
+        while (i < left && j < right) {
+            if (arrayLeft[i] <= arrayRight[j]) {
+                array[k++] = arrayLeft[i++];
+            }
+            else {
+                array[k++] = arrayRight[j++];
+            }
+        }
+        while (i < left) {
+            array[k++] = arrayLeft[i++];
+        }
+        while (j < right) {
+            array[k++] = arrayRight[j++];
+        }
+
+        return array;
+    }
+}
