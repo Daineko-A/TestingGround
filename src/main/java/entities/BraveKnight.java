e59a5d43c18722a835a95da7952a6aad01314d7f
adajneko <adajneko@cotvec.com>
2025-12-24 13:08:50 +0300

    1

--- src/main/java/entities/BraveKnight.java
+++ src/main/java/entities/BraveKnight.java
@@ -1,11 +1,18 @@
 package entities;
 
 public class BraveKnight implements Fighter {
+    private String name;
+    private Weapon weapon;
+
     @Override
     public void fight() {
     }
 
     protected void eat() {
 
+    }
+
+    private static class Weapon {
+        private String name;
     }
 }
